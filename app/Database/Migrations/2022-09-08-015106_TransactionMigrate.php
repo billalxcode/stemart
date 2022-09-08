6e4707466709da90d9eaf841389495ecdcad2214
billalxcode <billal.xcode@gmail.com>
2022-09-08 09:56:11 +0700

    Commit 09.55 8 Sep 2022

---
+++ app/Database/Migrations/2022-09-08-015106_TransactionMigrate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TransactionMigrate extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
