cf54edd56a35457ab8449cd9af1a802a295def55
billalxcode <billal.xcode@gmail.com>
2022-09-10 14:22:06 +0700

    Commit 14.21 10 sep 2022

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -7,14 +7,47 @@ use App\Controllers\BaseController;
 class Auth extends BaseController
 {
     protected $userModel;
+    protected $session;
 
     function __construct()
     {
-        $this->userModel = new \APp\Models\UserModel();
+        $this->userModel = new \App\Models\UserModel();
+        $this->session = \Config\Services::session();
     }
     
     public function login()
     {
-        return $this->renderOnce('auth/login');
+        helper('form');
+
+        $method = $this->request->getMethod();
+        if ($method == 'post') {
+            $username = $this->request->getPost("username");
+            $password = $this->request->getPost("password");
+
+            $customer_data = $this->userModel->get_customer_by_username($username);
+
+            if ($customer_data && $customer_data['role'] == 'customer') { 
+                if (password_verify($password, $customer_data['password'])) {
+                    $refresh_token = $this->userModel->update_refresh_token($customer_data['id']);
+
+                    $data_session = [
+                        'logged' => true,
+                        'token' => $refresh_token
+                    ];
+
+                    $this->session->set($data_session);
+
+                    return redirect()->to('products');
+                } else {
+                    $this->session->setFlashdata('error', 'Maaf username atau password salah');
+                    return redirect()->back()->withInput();
+                }
+            } else {
+                $this->session->setFlashdata('error', 'Maaf username atau password salah');
+                return redirect()->back()->withInput();
+            }
+        } else {
+            return $this->renderOnce('auth/login');
+        }
     }
 }
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -10,25 +10,45 @@
     <link rel="stylesheet" href="<?= base_url('assets/native/css/login.css') ?>">
 </head>
 <body class="login-bg">
-    <div class="container d-flex justify-content-center">
-        <div class="login-card-box p-5">
-            <h4 class="login-card-title">
-                <span class="green">Login</span> to your account
-            </h4>
-            <form action="" method="post" class="mt-5">
-                <div class="mb-3">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="username" placeholder="Masukan username">
+    <div class="container d-block justify-content-center align-items-center">
+        <div class="row">
+            <div class="col-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="login-card-box p-5 mx-auto">
+                            <h4 class="login-card-title">
+                                <span class="green">Login</span> to your account
+                            </h4>
+                            <form action="<?= base_url('login') ?>" method="post" class="mt-5">
+                                <?php if (session()->getFlashdata('error')): ?>
+                                    <div class="form-group">
+                                        <div class="alert alert-danger">
+                                            <?= session()->getFlashdata('error') ?>
+                                        </div>
+                                    </div>
+                                <?php endif ?>
+                                <div class="form-group mb-3">
+                                    <label for="username" class="form-label">Username</label>
+                                    <input type="text" class="form-control" id="username" placeholder="Masukan username" name="username" value="<?= old('username') ?>" autofocus>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="text" class="form-control" id="password" placeholder="Masukan password" name="password">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <button class="mt-4">Login</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="text" class="form-control" id="password" placeholder="Masukan password">
-                </div>
-                <div class="mb-3">
-                    <button class="mt-4">Login</button>
-                </div>
-            </form>
+            </div>
+            <div class="col-12 mt-3">
+                    <p class="text-center">Belum punya akun? <a href="<?= base_url('register') ?>">Daftar disini</a></p>
+                
+            </div>
         </div>
+
     </div>
 </body>
 </html>
